26de0054e04547b3879e09aaebadb827c11170e8
Piyush Omprakash Nagdete <pnagdeote219@gmail.com>
2024-08-07 01:50:14 +0530

    Frist Commit

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import './Home.css'
-import Header from '../../components/Header/Header'
-import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
-import AppDownload from '../../components/AppDownload/AppDownload'
+import React, { useState } from 'react'; 
+import './Home.css';
+import Header from '../../components/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import AppDownload from '../../components/AppDownload/AppDownload';
 
 const Home = () => {
 
@@ -19,4 +19,5 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
+
